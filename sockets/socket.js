a8e5737ba9780bac1343833d1f93e76cf57b8881
leandro curra <leandro.curra@xcaleconsulting.com>
2023-02-05 12:25:19 -0300

    inicio

---
+++ sockets/socket.js
@@ -0,0 +1,10 @@
+
+const {io} = require('../index');
+// Socket messages
+io.on('connection', client => {
+    console.log( 'Cliente conectado' ) 
+    client.on('disconnect', () => { console.log( 'Cliente desconectado' ) });
+    client.on('message', payload => {
+        io.emit( 'message', {admin: 'Nuevo Mensaje'} );
+    });
+});
